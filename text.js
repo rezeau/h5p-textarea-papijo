6aa41c548bdcbe6d56cfdfef52b58ec8e3983ec6
rezeau <joseph@rezeau.org>
2022-08-25 22:19:20 +0200

    included h5p tooltip
    Signed-off-by: rezeau <joseph@rezeau.org>

--- text.js
+++ text.js
@@ -11,29 +11,55 @@ H5P.TextareaPapiJo = (function ($, EventDispatcher) {
   function Textarea(parameters, id) {
     const self = this;
     EventDispatcher.call(this);
-
+    parameters.tipLabel = "Show tip";
+    let text = parameters.text;
     // If text was copied-pasted from another WYSIWYG editor we must clean extra line breaks and NOT CLEANED!
     if (parameters.removeExtraLineBreaks) {
       //This javascript code replaces all 3 types of line breaks with a single space
-      parameters.text = parameters.text.replace(/(\r\n|\n|\r)/gm, " ") ;
+      text = text.replace(/(\r\n|\n|\r)/gm, " ") ;
       //Replace all double white spaces with single spaces
-      parameters.text = parameters.text.replace(/\s+/g, " ");
+      text = text.replace(/\s+/g, " ");
     }
     // Now deal with cleaned text at https://www.textfixer.com/tools/remove-line-breaks.php
-    parameters.text = parameters.text.replace(/(\r\n|\n|\r)/gm, "<br>");
+    text = text.replace(/(\r\n|\n|\r)/gm, "<br>");
 
     //Allow some html tags.
-    parameters.text = parameters.text.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
-    const html = (parameters.text === undefined ? '<em>New text</em>' : parameters.text);
-
-    /**
-     * Wipe container and add text html.
-     *
-     * @alias H5P.AdvancedText#attach
-     * @param {H5P.jQuery} $container
-     */
+    text = text.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+    const DUMMYCHARACTER = '\u200B'; // zero-width space character
+    const parseText = text.split(/(\*.*?\*)/);
     self.attach = function ($container) {
-      $container.addClass('h5p-textarea-pj').html(html);
+      $container.addClass('h5p-textarea-pj');
+      parseText
+        .forEach(function (part) {
+          if ((part.substr(0, 1) === '*') && (part.substr(-1) === '*') && (part.indexOf(':') > -1)) {
+            // word has tip
+            let tip = part.match(/(:([^\\*]+))/g);
+            if (tip) {
+              let partLen = part.length;
+              tip = tip[0].replace(':', '');
+              // If tip only contains a reference to an image, needs some character, so we add an invisible one.
+              if ((tip.substr(0, 4) === '<img')) {
+                tip = tip.replace('<img', DUMMYCHARACTER + '​<img');
+                partLen = partLen + 2;
+              }
+              let word = part.slice(1, partLen - tip.length - 2);
+              word = '<em>' + word + '</em>';
+              $container.append(word);
+              self.$tip = H5P.JoubelUI.createTip(tip, {
+                tipLabel: parameters.tipLabel,
+                tabcontrol: true
+              });
+              $container.append(self.$tip);
+            }
+          }
+          else {
+            // is normal text
+            const el = document.createElement('span');
+            el.innerHTML = text;
+            $container.append(part);
+          }
+        });
+
     };
 
   }
